9c8ffa70d697968f048298190e10e0708eac97e9
janesfer <sferrazzajane@gmail.com>
2024-02-25 22:48:55 -0500

    ADDED tailwind and daisy UI for styles

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,10 +1,38 @@
 import React from "react";
 import ProductCard from "../components/ProductCard";
+interface User {
+  id: number;
+  name: string;
+  email: string;
+}
 
-const UsersPage = () => {
-  return <div>Hello page</div>
-  <ProductCard />;
-  
+const UsersPage = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users", {
+    cache: "no-store",
+  });
+  const users: User[] = await res.json();
+
+  return (
+    <>
+      <h1>Users</h1>
+      <table className="table table-zebra">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
 };
 
 export default UsersPage;
